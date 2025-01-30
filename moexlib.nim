c2ca3b4ad1441969546f88598b8a73710a730fd4
gitsergio99 <chernikov1981sv@gmail.com>
2025-01-30 09:18:15 +0700

    modified:   moexlib.nim 	modified:   test.exe 	modified:   test.nim

--- moexlib.nim
+++ moexlib.nim
@@ -1,4 +1,4 @@
-import std/[times,parsecsv,httpclient,strutils,math,strformat,re]
+import std/[times,httpclient,strutils,math,strformat,re,streams]
 
 type
     Col* = object
@@ -28,7 +28,7 @@ proc slice_dates_from_request*(start_date:string, end_date:string):seq[array[2,s
         tmp_res_seq.add([tmp_date1.format("yyyy-MM-dd"),tmp_date2.format("yyyy-MM-dd")])
     return tmp_res_seq
 
-proc get_data_moex*(moex_ticket: string, dates:seq[array[2,string]]):seq[Col] =
+proc get_data_moex_all_colmn*(moex_ticket: string, dates:seq[array[2,string]]):seq[Col] =
     var
         tmp_data:seq[Col] = @[]
         http_c = newHttpClient()
@@ -51,6 +51,45 @@ proc get_data_moex*(moex_ticket: string, dates:seq[array[2,string]]):seq[Col] =
         for x in countup(0,len(tmp_data)-1):
             tmp_re_string = fmt"""(?<={tmp_data[x].name}\=")(.*?)(?="\s)"""
             tmp_data[x].data.add(tmp_req_content.findAll(re(tmp_re_string)))
-    
+    tmp_data.delete(len(tmp_data)-1)
+    tmp_data.delete(len(tmp_data)-1)
+    tmp_data.delete(len(tmp_data)-1)
     return tmp_data
         #echo tmp_req_content
+    
+proc get_data_moex_colmn*(moex_ticket: string, dates:seq[array[2,string]],colmn:seq[string]):seq[Col] =
+    var
+        tmp_data:seq[Col] = @[]
+        http_c = newHttpClient()
+        tmp_req:string = "https://iss.moex.com/iss/history/engines/stock/markets/shares/securities/"
+        tmp_req_content:string
+        tmp_re_string:string
+    for x in colmn:
+        tmp_data.add(Col(name: x,data: @[]))
+    for x in dates:
+        tmp_req = tmp_req&fmt"{moex_ticket}?from={x[0]}&till={x[1]}&marketprice_board=1"
+        tmp_req_content = http_c.getContent(tmp_req)
+        tmp_req = "https://iss.moex.com/iss/history/engines/stock/markets/shares/securities/"
+        for x in countup(0,len(tmp_data)-1):
+            tmp_re_string = fmt"""(?<={tmp_data[x].name}\=")(.*?)(?="\s)"""
+            tmp_data[x].data.add(tmp_req_content.findAll(re(tmp_re_string)))        
+    return tmp_data
+
+proc save_data_to_csv*(data:seq[Col], file_name:string) =
+    var
+        csv_file = newFileStream(file_name,fmWrite)
+        tmp_string_line:string = ""
+    for x in data:
+        tmp_string_line = tmp_string_line&x.name&","
+    tmp_string_line.delete(len(tmp_string_line)-1..len(tmp_string_line)-1)
+    csv_file.writeLine(tmp_string_line)
+    tmp_string_line = ""
+    for x in countup(0,len(data[0].data)-1):
+        for y in data:
+            tmp_string_line = tmp_string_line&y.data[x]&","
+        tmp_string_line.delete(len(tmp_string_line)-1..len(tmp_string_line)-1)
+        csv_file.writeLine(tmp_string_line)
+        tmp_string_line = ""
+    csv_file.close()
+
+
--- test.nim
+++ test.nim
@@ -1,18 +1,12 @@
 import times,math, moexlib
 var http_data_table:seq[Col]
-let st_dt = parse("2024-10-01","yyyy-MM-dd")
-let end_dt = parse("2024-12-01","yyyy-MM-dd")
-echo (st_dt - end_dt).inDays
-echo st_dt.format("yyyy'+'MM-dd hh:mm:ss")
-var ds: TimeInterval
-ds.days =10
-let x = 285
-var y:int = 0
-y = int((x/100).ceil)
-echo y
-echo st_dt + ds
+var clmn:seq[string] = @["TRADEDATE","OPEN","HIGH","LOW","CLOSE","LEGALCLOSEPRICE","VOLUME"]
 
 #echo slice_dates_from_request("2024-01-01","2024-12-26")
 
-http_data_table = get_data_moex("LSNG",slice_dates_from_request("2024-01-01","2024-10-10"))
+#http_data_table = get_data_moex_all_colmn("LSNG",slice_dates_from_request("2024-01-01","2024-10-10"))
+#echo http_data_table
+
+http_data_table = get_data_moex_colmn("LSNG",slice_dates_from_request("2024-01-01","2024-10-10"),clmn)
 echo http_data_table
+save_data_to_csv(http_data_table,"lsng.csv")
