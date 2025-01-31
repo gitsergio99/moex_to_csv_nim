48562da396a39bf2f2a15576f3f68bf9b1db74a6
gitsergio99 <chernikov1981sv@gmail.com>
2025-01-31 09:02:21 +0700

    modified:   test.nim

--- test.nim
+++ test.nim
@@ -1,7 +1,7 @@
 import times,math, moexlib
 #var for data from moex
 var http_data_table:seq[Col]
-#neccery columns 
+#necessary columns 
 var clmn:seq[string] = @["TRADEDATE","OPEN","HIGH","LOW","CLOSE","LEGALCLOSEPRICE","VOLUME"]
 
 #get data from moex, moex push back only 100 records per 1 request. So, slice_dates_from_request - slice request if it need.
